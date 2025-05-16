607517992205c2e961eda2f1204aa3b01ab1abed
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 08:34:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 3.0.0"
     }
   }
 }
-
 provider "aws" {
-  region = "us-east-1"
-}
+  region = var.aws_region
 
 module "vpc" {
   source = "./modules/vpc"
