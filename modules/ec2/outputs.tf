23bb6a89e70d8ec850eb56a81f6e503e89bd5dc6
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 08:44:54 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,19 +1,3 @@
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.main.public_ip
-}
-
 output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.main.id
-}
-
-output "instance_private_ip" {
-  description = "The private IP address of the EC2 instance"
-  value       = aws_instance.main.private_ip
-}
-
-output "instance_availability_zone" {
-  description = "The availability zone of the EC2 instance"
-  value       = aws_instance.main.availability_zone
+  value = aws_instance.my_aws_instance.id
 }
