f1d3d5210e06cf8d073adad227326482b4a8fbf5
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 08:27:58 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,17 @@
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance."
+  type        = string
+  default     = "first-key"
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+  default     = "ami-084568db4383264d4"
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "t3.medium"
+}
