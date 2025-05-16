b0d0c04e48a438a2629dc472af798debd21cc8ce
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 08:43:12 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,50 +1,48 @@
-resource "aws_lb" "openproject_alb" {
-  name               = "focal-board"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [var.alb_security_group_id]
-  subnets            = var.subnet_ids
+# Define the ALB
+resource "aws_lb" "alb" {
+    name               = "example-alb"
+    internal           = false
+    load_balancer_type = "application"
+    security_groups    = [var.alb_security_group_id]
+    subnets            = var.subnets
 
-  enable_deletion_protection = false
-
-  tags = {
-    Name = "focal-board-ALB"
-  }
+    enable_deletion_protection = false
 }
 
-resource "aws_lb_target_group" "openproject_tg" {
-  name     = "focal-board-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+# Define the Target Group with Health Check
+resource "aws_lb_target_group" "alb_target_group" {
+    name        = "example-target-group"
+    port        = 8080
+    protocol    = "HTTP"
+    vpc_id      = var.vpc_id
+    target_type = "instance"
 
-  health_check {
-    path                = "/login"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "OpenProject-TG"
-  }
+    health_check {
+        path                = "/login"
+        port                = "8080"
+        protocol            = "HTTP"
+        interval            = 30
+        timeout             = 5
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+    }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.openproject_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+# Define the Listener
+resource "aws_lb_listener" "alb_listener" {
+    load_balancer_arn = aws_lb.alb.arn
+    port              = 80
+    protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.openproject_tg.arn
-  }
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.alb_target_group.arn
+    }
 }
 
-resource "aws_lb_target_group_attachment" "openproject" {
-  target_group_arn = aws_lb_target_group.openproject_tg.arn
+# Example: Attach EC2 instances to the Target Group
+resource "aws_lb_target_group_attachment" "example" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
   target_id        = var.instance_id
   port             = 8080
 }
-
