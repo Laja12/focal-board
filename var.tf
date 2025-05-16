2ed7d2d19a9a9cb6701c9c56a7f9916844d8a7f8
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 08:51:35 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,17 +1,19 @@
-variable "key_name" {
-  description = "The name of the key pair to use for the EC2 instance."
-  type        = string
-  default     = "first-key"
-}
-
-variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
+variable "ami_value" {
+  description = "value of ami"
   type        = string
   default     = "ami-084568db4383264d4"
+  
 }
 
 variable "instance_type" {
-  description = "Instance type"
+  description = "Type of instance to create"
   type        = string
   default     = "t3.medium"
+  
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance"
+  type        = string
+  default     = "aws-login"
 }
