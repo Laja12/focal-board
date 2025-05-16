d609e04d6736553a112d9b818fbe16ace3bfbb42
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 08:49:43 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,7 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.my-vpc.id
 }
 
-output "subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "route_table_id" {
-  value = aws_route_table.public.id
+output "subnet_ids"{
+  value = [aws_subnet.public_subnet.id, aws_subnet.public_subnet_2.id]
 }
