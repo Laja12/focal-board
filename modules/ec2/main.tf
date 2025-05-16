502eead81fd3c0a17d04f975eb18a5b64827e2c7
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 08:44:25 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,10 @@
-resource "aws_instance" "main" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-  key_name      = var.key_name
+resource "aws_instance" "my_aws_instance" {
+  ami                    = var.ami_value
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
+  associate_public_ip_address = true
+  key_name               = var.key_name
 
   root_block_device {
     volume_size = 15
@@ -11,13 +12,19 @@ resource "aws_instance" "main" {
   }
 
   user_data = <<-EOF
-               #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              docker run -d --name focalboard -p 8000:8000 mattermost/focalboard
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update -y
+              sudo apt-get install -y docker-ce
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo docker run -d --name focalboard -p 8000:8000 mattermost/focalboard
               EOF
+
   tags = {
-    Name = "main-instance"
+    Name = "my-aws-instance"
   }
 }
