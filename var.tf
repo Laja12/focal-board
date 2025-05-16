096c28eb08d5e25b5790f398bf22904a99d69881
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-16 09:02:54 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -15,5 +15,5 @@ variable "instance_type" {
 variable "key_name" {
   description = "The name of the key pair to use for the EC2 instance"
   type        = string
-  default     = "aws-login"
+  default     = "first-key"
 }
